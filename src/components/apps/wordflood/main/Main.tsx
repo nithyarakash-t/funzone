f1d7d289fe3f7dc8b084cfd1f78307b238ec1811
nithyaprakash-t <nithyaprakash.t@gmail.com>
2025-05-02 15:56:20 +0530

    added basic logic for checking and scoring

--- src/components/apps/wordflood/main/Main.tsx
+++ src/components/apps/wordflood/main/Main.tsx
@@ -1,4 +1,5 @@
 import { Fragment, useEffect, useState } from 'react';
+import wordList from 'an-array-of-english-words';
 import './Main.scss';
 
 const INITIAL_MATRIX_STATE:(string | null)[][] = [
@@ -9,7 +10,6 @@ const INITIAL_MATRIX_STATE:(string | null)[][] = [
     [null, null, null, null, null, null],
     [null, null, null, null, null, null]
 ]
-
 const TEST_MATRIX_STATE:(string | null)[][] = [
     ['a', 'b', 'c', 'e', 'x', 'z'],
     ['l', 'a', 't', 't', 'p', 'o'],
@@ -18,27 +18,57 @@ const TEST_MATRIX_STATE:(string | null)[][] = [
     ['f', 'g', 'h', 'j', 'l', 'k'],
     ['a', 'x', 'c', 'v', 'b', 'n']
 ]
+const WORD_SET = new Set(wordList);
+const MULTIPLIER = 10; //10 points per letter in word
 
 export function Main() {
-    const [matrix, setMatrix] = useState(TEST_MATRIX_STATE)
+    const [matrix, setMatrix] = useState(TEST_MATRIX_STATE);
     const [queue, setQueue] = useState<string[]>([]);
+    const [foundWords, setFoundWords] = useState<string[]>([]);
+    const [score, setScore] = useState(0);
 
     useEffect(()=>{
         console.log(queue, 'queue update')
     }, [queue])
 
 
+    function startGame() {
+
+    }
     function resetGame() {
         setMatrix(INITIAL_MATRIX_STATE);
     }
     function clearQueue() {
         setQueue([]);
     }
+
     function submitWord() {
+        if(queue.length < 1) return;
 
-    }
-    function startGame() {
+        const WORD = queue
+                    .map(rowcell => {
+                        const [row, col] = rowcell.split('-').map(Number);
+                        return matrix[row][col];
+                    })
+                    .join('');
+        const FLAG = isValidEnglishWord(WORD);
+        console.log(FLAG);
 
+        if(FLAG) {
+            const NEW_MATRIX = structuredClone(matrix);
+            queue.forEach((rowcell)=>{
+                const [row, col] = rowcell.split('-').map(Number);
+                NEW_MATRIX[row][col] = null;
+            })
+
+            setFoundWords([...foundWords, WORD]);
+            setScore((prev)=>prev + (WORD.length * MULTIPLIER));
+            setMatrix(NEW_MATRIX);
+            clearQueue();
+        }
+        else {
+            console.log('Not a word dumbass, try harder');
+        }
     }
     function handleCellClick(input:(string)) {
         // const [ROW_INDEX, CELL_INDEX] = input.split('-');
@@ -55,6 +85,11 @@ export function Main() {
 
         setQueue(newQueue);
     }
+
+    //utils
+    function isValidEnglishWord(word: string): boolean {
+        return WORD_SET.has(word.toLowerCase());
+    }
      return(
         <div className='wf-app__main'>
             <div className='wf-grid__table'>
@@ -63,12 +98,12 @@ export function Main() {
                     <tbody>
                         {matrix.map((row, rowIndex)=>{
                             return <tr key={rowIndex}>
-                                {row.map((cell, cellIndex)=>{
+                                {row.map((cellValue, cellIndex)=>{
                                     const ROW_CELL = `${rowIndex}-${cellIndex}`;
                                     return <td key={cellIndex}>
                                         <div className='wf-grid__table-cell'>
-                                            <button type='button' className='wf-grid__table-button' aria-label='Select - lorem' aria-selected={queue.indexOf(ROW_CELL) !== -1} onClick={()=>handleCellClick(ROW_CELL)}>
-                                                {cell}
+                                            <button type='button' className='wf-grid__table-button' aria-label='Select - lorem' disabled={cellValue === null ? true : undefined} aria-selected={queue.indexOf(ROW_CELL) !== -1} onClick={()=>handleCellClick(ROW_CELL)}>
+                                                {cellValue}
                                             </button>
                                         </div>
                                     </td>
@@ -92,6 +127,12 @@ export function Main() {
                 <button type='button' className='wf-app__start' aria-label='start' onClick={startGame}>Start Game</button>
                 <button type='button' className='wf-app__reset' aria-label='Reset' onClick={resetGame}>Restart Game</button>
             </div>
+            <p className='wf-app__foundwords'>
+                {foundWords.map((item,ind)=>{
+                    return <Fragment key={ind}>{item}, </Fragment>
+                })}
+            </p>
+            <p>{score}</p>
         </div>
     )
 }
--- src/components/apps/wordflood/references.ts
+++ src/components/apps/wordflood/references.ts
@@ -3,3 +3,12 @@ import { Reference } from "../../layout/references/references-list";
 export const references:Reference[] = [
    
 ]
+
+
+/**
+ * english-words vs an-array-of-english-words
+ * timer
+ * game modes - to death, set targets
+ * game difficulty - 1. progressive option 2. frequency 3. less vowels ?!, more z, y etc,. ?
+ *                 --------- in easy mode vowels and consonants should have similar probabil;ity  
+ */
