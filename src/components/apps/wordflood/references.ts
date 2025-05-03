42629a7c52154c9bd98ef32c93762682b1a1e374
nithyaprakash-t <nithyaprakash.t@gmail.com>
2025-05-03 23:48:21 +0530

    minor updates

--- src/components/apps/wordflood/references.ts
+++ src/components/apps/wordflood/references.ts
@@ -9,18 +9,13 @@ export const references:Reference[] = [
 
 
 /**
- * throttle interval when word check is happening --- 
+ * throttle interval when word check is happening --- DONE
+ * timer --- DONE
+ * How to play --- 
+ * game modes - to death, set targets --- DONE
+ * difficulty --- DONE
  * 
  * english-words vs an-array-of-english-words
- * timer
- * game modes - to death, set targets
  * game difficulty - 1. progressive option 2. frequency 3. less vowels ?!, more z, y etc,. ?
  *                 --------- in easy mode vowels and consonants should have similar probabil;ity  
- */
-
-/**
- * modes -
- *    - one off
- *    - keeps appearing
- *     
  */
